5a08933727b5227daefb3ca04a071b68632c78ee
OSS0302 <tkdtn3264@gmail.com>
2024-01-25 18:03:04 +0900

    로그인 페이지 뼈대 작성

---
+++ lib/presentation/sign_in/sign_in_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  SignInScreen({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('로그인 페이지'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjIfxtJWjgQh0Q_MNtzM9PnnKK6_Otv1X71g&usqp=CAU'),
+              Text('로그인',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40),),
+              input('이메일'),
+              SizedBox(height: 10,),
+              input('패스워드'),
+              buttomWidget()
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget input(String text) {
+  final _formKey = GlobalKey<FormState>();
+  return Form(
+    key: _formKey,
+    child: TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) {
+          return '이메일 을입력해주세요';
+        }
+      },
+      obscureText: false,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: text,
+        hintText: '이메일을 입력하세요',
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.blue,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(0),
+        ),
+      ),
+    ),
+  );
+}
+Widget buttomWidget () {
+  return Column(
+    children: [
+      Container(
+        width: 300,
+        height: 30,
+          child: ElevatedButton(onPressed: () {}, child: Text('로그인'))),
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          width: 200,
+          height: 30,
+            child: ElevatedButton(onPressed: () {}, child: Text('회원가입'))),
+      ),
+    ],
+  );
+}
