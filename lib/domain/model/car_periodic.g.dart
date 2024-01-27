a93a14227b9a2792a10d70c340672f0c7ec458dc
OSS0302 <tkdtn3264@gmail.com>
2024-01-27 17:05:13 +0900

    잡단한것 푸시

---
+++ lib/domain/model/car_periodic.g.dart
@@ -0,0 +1,79 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'car_periodic.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CarPeriodicImpl _$$CarPeriodicImplFromJson(Map<String, dynamic> json) =>
+    _$CarPeriodicImpl(
+      engineOil: json['engineOil'] as String? ?? "",
+      cleaningEngineFlushing: json['cleaningEngineFlushing'] as String? ?? "",
+      coilSparkPlug: json['coilSparkPlug'] as String? ?? "",
+      beBelSparkPlug: json['beBelSparkPlug'] as String? ?? "",
+      cleaningCombustionChamberFlushing:
+          json['cleaningCombustionChamberFlushing'] as String? ?? "",
+      preheatingHeatingPlug: json['preheatingHeatingPlug'] as String? ?? "",
+      cleaingIntakeLineFlushing:
+          json['cleaingIntakeLineFlushing'] as String? ?? "",
+      timingBelt: json['timingBelt'] as String? ?? "",
+      waterPumpBearingSet: json['waterPumpBearingSet'] as String? ?? "",
+      externalDriveBelt: json['externalDriveBelt'] as String? ?? "",
+      engineTransmissionMounting:
+          json['engineTransmissionMounting'] as String? ?? "",
+      transmissionOil: json['transmissionOil'] as String? ?? "",
+      differentialOil: json['differentialOil'] as String? ?? "",
+      transferOil: json['transferOil'] as String? ?? "",
+      fuelFilter: json['fuelFilter'] as String? ?? "",
+      fuelInjectorCleaning: json['fuelInjectorCleaning'] as String? ?? "",
+      brakePad: json['brakePad'] as String? ?? "",
+      brakeOil: json['brakeOil'] as String? ?? "",
+      brakeDiscPolishing: json['brakeDiscPolishing'] as String? ?? "",
+      antifreeze: json['antifreeze'] as String? ?? "",
+      cleaingCoolingLineFlushing:
+          json['cleaingCoolingLineFlushing'] as String? ?? "",
+      Filter: json['Filter'] as String? ?? "",
+      cleaningAirConditionerHeater:
+          json['cleaningAirConditionerHeater'] as String? ?? "",
+      airConditionerRefrigerantInspection:
+          json['airConditionerRefrigerantInspection'] as String? ?? "",
+      powerSteeringOil: json['powerSteeringOil'] as String? ?? "",
+      wheelAlignment: json['wheelAlignment'] as String? ?? "",
+      battery: json['battery'] as String? ?? "",
+      lowerBodyUndercoating: json['lowerBodyUndercoating'] as String? ?? "",
+    );
+
+Map<String, dynamic> _$$CarPeriodicImplToJson(_$CarPeriodicImpl instance) =>
+    <String, dynamic>{
+      'engineOil': instance.engineOil,
+      'cleaningEngineFlushing': instance.cleaningEngineFlushing,
+      'coilSparkPlug': instance.coilSparkPlug,
+      'beBelSparkPlug': instance.beBelSparkPlug,
+      'cleaningCombustionChamberFlushing':
+          instance.cleaningCombustionChamberFlushing,
+      'preheatingHeatingPlug': instance.preheatingHeatingPlug,
+      'cleaingIntakeLineFlushing': instance.cleaingIntakeLineFlushing,
+      'timingBelt': instance.timingBelt,
+      'waterPumpBearingSet': instance.waterPumpBearingSet,
+      'externalDriveBelt': instance.externalDriveBelt,
+      'engineTransmissionMounting': instance.engineTransmissionMounting,
+      'transmissionOil': instance.transmissionOil,
+      'differentialOil': instance.differentialOil,
+      'transferOil': instance.transferOil,
+      'fuelFilter': instance.fuelFilter,
+      'fuelInjectorCleaning': instance.fuelInjectorCleaning,
+      'brakePad': instance.brakePad,
+      'brakeOil': instance.brakeOil,
+      'brakeDiscPolishing': instance.brakeDiscPolishing,
+      'antifreeze': instance.antifreeze,
+      'cleaingCoolingLineFlushing': instance.cleaingCoolingLineFlushing,
+      'Filter': instance.Filter,
+      'cleaningAirConditionerHeater': instance.cleaningAirConditionerHeater,
+      'airConditionerRefrigerantInspection':
+          instance.airConditionerRefrigerantInspection,
+      'powerSteeringOil': instance.powerSteeringOil,
+      'wheelAlignment': instance.wheelAlignment,
+      'battery': instance.battery,
+      'lowerBodyUndercoating': instance.lowerBodyUndercoating,
+    };
