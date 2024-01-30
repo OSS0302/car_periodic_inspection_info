8c96736c2ee1ffeac1ab21bc3e081cf10daec3b8
OSS0302 <tkdtn3264@gmail.com>
2024-01-30 15:45:55 +0900

    슈퍼베이스로  회원가입 구현 하기

--- lib/presentation/sign_up/sign_up_screen.dart
+++ lib/presentation/sign_up/sign_up_screen.dart
@@ -1,9 +1,32 @@
 import 'package:car_periodic_inspection_info/presentation/car_main_screen/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class SignUpScreen extends StatelessWidget {
+final supabase = Supabase.instance.client;
+
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController idController = TextEditingController();
+  TextEditingController pwController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+
+  @override
+  void dispose() {
+    idController.dispose();
+    pwController.dispose();
+    nameController.dispose();
+    phoneController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,51 +38,97 @@ class SignUpScreen extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            signUpInfo('아이디'),
-            SizedBox(height: 20,),
-            signUpInfo('패스워드'),
-            SizedBox(height: 20,),
-            signUpInfo('패스워드 확인 '),
-            SizedBox(height: 20,),
-            signUpInfo('이름'),
-            SizedBox(height: 20,),
-            signUpInfo('전화번호'),
-            SizedBox(height: 20,),
+            TextFormField(
+              controller: idController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                labelText: '이메일',
+                hintText: '이메일',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            TextFormField(
+              controller: pwController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                labelText: 'pw',
+                hintText: 'pw',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            TextFormField(
+              controller: nameController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                labelText: '이름을 입력해 주세요',
+                hintText: '이름',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            TextFormField(
+              controller: phoneController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                labelText: '전화번호 입력해 주세요',
+                hintText: '전화번호',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
             SizedBox(
-              width: 200,
-              height: 60,
-                child: FloatingActionButton(onPressed: () => context.go('/mainScreen'),
-
-                  child: Text('가입 완료',style: TextStyle(fontSize: 30),),))
+                width: 200,
+                height: 60,
+                child: FloatingActionButton(
+                  onPressed: () async {
+                    context.go('/mainScreen');
+                    final AuthResponse res = await supabase.auth.signUp(
+                      email: idController.text,
+                      password: pwController.text,
+                      data: {
+                        'username': nameController.text,
+                        'phonenumber': phoneController.text,
+                      },
+                    );
+                  },
+                  child: Text(
+                    '가입 완료',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ))
           ],
         ),
       ),
-      
     );
   }
-}
-
-Widget signUpInfo(String text) {
-  return TextFormField(
-    validator: (value) {
-      if (value!.isEmpty) {
-        return '이메일 을입력해주세요';
-      }
-    },
-    obscureText: false,
-    decoration: InputDecoration(
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16)
-      ),
-      labelText: text,
-      hintText: text,
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Colors.blue,
-          width: 2,
-        ),
-        borderRadius: BorderRadius.circular(16),
-      ),
-    ),
-  );
 }
