f3cf7f5d42b27e226176e89b694f1aae0e068e73
OSS0302 <tkdtn3264@gmail.com>
2024-01-30 15:46:13 +0900

    슈퍼베이스로  로그인 구현 하기

--- lib/presentation/sign_in/sign_in_screen.dart
+++ lib/presentation/sign_in/sign_in_screen.dart
@@ -2,10 +2,24 @@ import 'package:car_periodic_inspection_info/presentation/car_main_screen/main_s
 import 'package:car_periodic_inspection_info/presentation/sign_up/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
   SignInScreen({super.key});
 
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController idController = TextEditingController();
+  TextEditingController pwController = TextEditingController();
+  @override
+  void dispose() {
+    idController.dispose();
+    pwController.dispose();
+    super.dispose();
+  }
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -22,10 +36,66 @@ class SignInScreen extends StatelessWidget {
               Image.network(
                   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjIfxtJWjgQh0Q_MNtzM9PnnKK6_Otv1X71g&usqp=CAU'),
               const Text('로그인',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40),),
-              input('이메일'),
-              SizedBox(height: 10,),
-              input('패스워드'),
-              buttomWidget(context)
+              TextFormField(
+                controller: idController,
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16)
+                  ),
+                  labelText: '이메일',
+                  hintText: '이메일 입력해주세요',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+              TextFormField(
+                controller: pwController,
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16)
+                  ),
+                  labelText: 'pw',
+                  hintText: '비밀번호를 입력해주세요',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+              Container(
+                  width: 300,
+                  height: 30,
+                  child: ElevatedButton(onPressed: ()  async {
+
+                    context.go('/mainScreen');
+                    final AuthResponse res = await supabase.auth.signInWithPassword(
+                      email: idController.text,
+                      password: pwController.text,
+                    );
+                  },
+
+                      child: Text('로그인'))),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                    width: 200,
+                    height: 30,
+                    child: ElevatedButton(onPressed: () {
+                      context.go('/SignUpScreen');
+
+
+                    }, child: Text('회원가입'))),
+              ),
 
             ],
           ),
@@ -35,54 +105,4 @@ class SignInScreen extends StatelessWidget {
   }
 }
 
-Widget input(String text) {
-  final _formKey = GlobalKey<FormState>();
-  return Form(
-    key: _formKey,
-    child: TextFormField(
-      validator: (value) {
-        if (value!.isEmpty) {
-          return '이메일 을입력해주세요';
-        }
-      },
-      obscureText: false,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: text,
-        hintText: '이메일을 입력하세요',
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.blue,
-            width: 2,
-          ),
-          borderRadius: BorderRadius.circular(16),
-        ),
-      ),
-    ),
-  );
-}
-Widget buttomWidget (BuildContext context) {
-  return Column(
-    children: [
-      Container(
-        width: 300,
-        height: 30,
-          child: ElevatedButton(onPressed: () => context.go('/mainScreen'),
 
-          child: Text('로그인'))),
-      Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Container(
-          width: 200,
-          height: 30,
-            child: ElevatedButton(onPressed: () {
-              Navigator.push(
-                context,
-                  MaterialPageRoute(
-                    builder: (context) => SignUpScreen()),
-              );
-            }, child: Text('회원가입'))),
-      ),
-    ],
-  );
-}
