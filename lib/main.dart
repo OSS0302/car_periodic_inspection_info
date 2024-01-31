53d485a0a24264be057a30506811d5fd4a746a4c
OSS0302 <tkdtn3264@gmail.com>
2024-01-31 17:46:25 +0900

    사용자가 입력 키보드 누르면 키보드 사라지기

--- lib/main.dart
+++ lib/main.dart
@@ -26,15 +26,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: MaterialApp.router(
+        routerConfig: router,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+
+
       ),
-
-
     );
   }
 }
