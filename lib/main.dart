b1967a966ebdb103b1bd59bc0297faac1c839ab5
OSS0302 <tkdtn3264@gmail.com>
2024-01-31 17:20:21 +0900

    파이어베이이스에서 슈퍼베이스로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 
-import 'package:car_periodic_inspection_info/firebase_options.dart';
 import 'package:car_periodic_inspection_info/presentation/ui/routes.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+// WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//       options: DefaultFirebaseOptions.currentPlatform,
+//   );
 
 
-
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+
+  await Supabase.initialize(
+    url: 'https://krwrbohnxnwqgalrheaf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtyd3Jib2hueG53cWdhbHJoZWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1ODQzNTgsImV4cCI6MjAyMjE2MDM1OH0.KFgPaCHEI1iTiyUOyquaQkD4nh4pO23PyL0qv9pX0zE',
   );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
