3e86029ff3a77008e8cd63434bef14d42986f150
OSS0302 <tkdtn3264@gmail.com>
2024-02-01 15:12:47 +0900

    차량 입력 정보 데이터를 슈퍼베이스에 추가하기

--- lib/presentation/car_info_add_screen/car_info_add_screen.dart
+++ lib/presentation/car_info_add_screen/car_info_add_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final supabase = Supabase.instance.client;
 
 class CarInfoAddScreen extends StatefulWidget {
   const CarInfoAddScreen({super.key});
@@ -9,11 +13,21 @@ class CarInfoAddScreen extends StatefulWidget {
 }
 
 class _CarInfoAddScreenState extends State<CarInfoAddScreen> {
-  TextEditingController textController = TextEditingController();
+  final stream = supabase.from('user_info').stream(primaryKey: ['id']);
+
+  TextEditingController companyController = TextEditingController();
+  TextEditingController carSelectController = TextEditingController();
+  TextEditingController gasSelectController = TextEditingController();
+  TextEditingController distanceController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
 
   @override
   void dispose() {
-    textController.dispose();
+    companyController.dispose();
+    carSelectController.dispose();
+    gasSelectController.dispose();
+    distanceController.dispose();
+    dateController.dispose();
     super.dispose();
   }
 
@@ -21,21 +35,86 @@ class _CarInfoAddScreenState extends State<CarInfoAddScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: const Text('차량 입력 정보 '),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              dropDownMenu(),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          carInspectionInput(),
-           buttom(context),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40)),
+                        child: dropDownMenu()),
+                  ],
+                ),
+                TextField(
+                  controller: companyController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: '제조회사',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: carSelectController,
+                  decoration: InputDecoration(
+                    hintText: '차량선택',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: gasSelectController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: '연료유형',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: distanceController,
+                  decoration: InputDecoration(
+                    hintText: '주행한 키로수',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+
+                Container(
+                    width: 300,
+                    height: 50,
+                    child: ElevatedButton(
+                        onPressed: () async{
+                          context.go('/mainScreen');
+                          await supabase.from('car_periodic_add').insert({
+                            'company': companyController.text ?? '',
+                            'car_select':carSelectController.text ?? '',
+                            'gas_select': gasSelectController.text?? '',
+                            'distance':distanceController.text?? '',
+                            'date':  DateFormat("yyyy-MM-dd ").format(DateTime.now()),
+                          });
+
+                        },
+                        child: Text('작성 완료'))),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -47,7 +126,8 @@ Widget dropDownMenu() {
     child: Padding(
       padding: EdgeInsets.symmetric(vertical: 8.0),
       child: DropdownMenu(
-        label: Text('차량 선택 하기 '),
+        width: 200,
+        label: Text(' 차 선택 '),
         dropdownMenuEntries: [
           DropdownMenuEntry(value: Colors.orange, label: '1대'),
           DropdownMenuEntry(value: Colors.green, label: '2대'),
@@ -56,71 +136,6 @@ Widget dropDownMenu() {
           DropdownMenuEntry(value: Colors.lime, label: '5대'),
         ],
       ),
-    ),
-  );
-}
-
-Widget carInspectionInput() {
-  return const Padding(
-    padding: EdgeInsets.all(8.0),
-    child: Column(
-      children: [
-        TextField(
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            hintText: '제조회사',
-            border: OutlineInputBorder(),
-          ),
-        ),
-        SizedBox(height: 10,),
-        TextField(
-          decoration: InputDecoration(
-            hintText: '차량선택',
-            border: OutlineInputBorder(),
-          ),
-        ),
-        SizedBox(height: 10,),
-        TextField(
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            hintText: '연료유형',
-            border: OutlineInputBorder(),
-          ),
-        ),
-        SizedBox(height: 10,),
-        TextField(
-          decoration: InputDecoration(
-            hintText: '주행한 키로수',
-            border: OutlineInputBorder(),
-          ),
-        ),
-        SizedBox(height: 10,),
-        TextField(
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            hintText: '점검한 날짜',
-            border: OutlineInputBorder(),
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-
-
-
-Widget buttom(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          width: 300,
-          height: 50,
-            child: ElevatedButton(onPressed: () => context.go('/mainScreen'), child: Text('작성 완료'))),
-      ],
     ),
   );
 }
