dcd515e70df0781fb3be1026a0ffa99c98f5891e
OSS0302 <tkdtn3264@gmail.com>
2024-02-01 10:54:28 +0900

    api key 가리기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:car_periodic_inspection_info/key.dart';
 import 'package:car_periodic_inspection_info/presentation/ui/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -14,7 +15,7 @@ Future<void> main() async {
 
   await Supabase.initialize(
     url: 'https://krwrbohnxnwqgalrheaf.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtyd3Jib2hueG53cWdhbHJoZWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1ODQzNTgsImV4cCI6MjAyMjE2MDM1OH0.KFgPaCHEI1iTiyUOyquaQkD4nh4pO23PyL0qv9pX0zE',
+    anonKey: key,
   );
   runApp(const MyApp());
 }
