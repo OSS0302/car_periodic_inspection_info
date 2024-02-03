f473d349abd6a57dbb6d2e318e2be67772645658
OSS0302 <tkdtn3264@gmail.com>
2024-02-03 20:37:43 +0900

    회원가입 페이지 에 유효성 검사 로직 추가하기

--- lib/presentation/sign_up/sign_up_screen.dart
+++ lib/presentation/sign_up/sign_up_screen.dart
@@ -5,13 +5,15 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 final supabase = Supabase.instance.client;
 
 class SignUpScreen extends StatefulWidget {
-  const SignUpScreen({super.key});
+  const SignUpScreen({Key? key}) : super(key: key);
 
   @override
   State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   TextEditingController idController = TextEditingController();
   TextEditingController pwController = TextEditingController();
   TextEditingController nameController = TextEditingController();
@@ -35,102 +37,153 @@ class _SignUpScreenState extends State<SignUpScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: idController,
-              obscureText: false,
-              decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
-                labelText: '이메일',
-                hintText: '이메일',
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                    width: 2,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '이메일을 입력하세요';
+                    }
+
+                    return null;
+                  },
+                  controller: idController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    labelText: '이메일',
+                    hintText: '이메일',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
                   ),
-                  borderRadius: BorderRadius.circular(16),
                 ),
               ),
-            ),
-            TextFormField(
-              controller: pwController,
-              obscureText: false,
-              decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
-                labelText: 'pw',
-                hintText: 'pw',
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                    width: 2,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '비밀번호를 입력하세요';
+                    }
+
+                    return null;
+                  },
+                  controller: pwController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    labelText: '비밀번호',
+                    hintText: '비밀번호',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
                   ),
-                  borderRadius: BorderRadius.circular(16),
                 ),
               ),
-            ),
-            TextFormField(
-              controller: nameController,
-              obscureText: false,
-              decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
-                labelText: '이름을 입력해 주세요',
-                hintText: '이름',
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                    width: 2,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '이름을 입력하세요';
+                    }
+
+                    return null;
+                  },
+                  controller: nameController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    labelText: '이름',
+                    hintText: '이름',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
                   ),
-                  borderRadius: BorderRadius.circular(16),
                 ),
               ),
-            ),
-            TextFormField(
-              controller: phoneController,
-              obscureText: false,
-              decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
-                labelText: '전화번호 입력해 주세요',
-                hintText: '전화번호',
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                    width: 2,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '전화번호를 입력하세요';
+                    }
+
+                    return null;
+                  },
+                  controller: phoneController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    labelText: '전화번호',
+                    hintText: '전화번호',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
                   ),
-                  borderRadius: BorderRadius.circular(16),
                 ),
               ),
-            ),
-            SizedBox(
+              SizedBox(
                 width: 200,
                 height: 60,
                 child: FloatingActionButton(
                   onPressed: () async {
-                    context.go('/');
-                    final AuthResponse res = await supabase.auth.signUp(
+                    if (_formKey.currentState!.validate()) {
+                      context.go('/');
+                      final AuthResponse res = await supabase.auth.signUp(
                         email: idController.text,
                         password: pwController.text,
                         data: {
                           'name': nameController.text,
-                          'phone': nameController.text,
+                          'phone': phoneController.text,
+                        },
+                      );
+                      if (res.user != null) {
+                        await supabase.from('user_info').insert({
+                          'name': nameController.text,
+                          'phone': phoneController.text,
                         });
-                    if(res.user != null) {
-                      await supabase.from('user_info').insert({
-                        'name': nameController.text,
-                        'phone': phoneController.text,
-                      });
+                      }
                     }
                   },
                   child: Text(
                     '가입 완료',
                     style: TextStyle(fontSize: 30),
                   ),
-                ))
-          ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
