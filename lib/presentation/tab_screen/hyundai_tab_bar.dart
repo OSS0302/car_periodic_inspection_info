342adcce30138954e638ee4e371a28cf460d9141
OSS0302 <tkdtn3264@gmail.com>
2024-02-06 22:56:33 +0900

    탭바 자료 추가하기

--- lib/presentation/tab_screen/hyundai_tab_bar.dart
+++ lib/presentation/tab_screen/hyundai_tab_bar.dart
@@ -1,24 +1,25 @@
 import 'package:car_periodic_inspection_info/presentation/tab_screen/tab_info.dart';
 import 'package:flutter/material.dart';
 
-class TabPage extends StatefulWidget {
-  const TabPage({Key? key}) : super(key: key);
+class HyundaiScreen extends StatefulWidget {
+  const HyundaiScreen({Key? key}) : super(key: key);
 
   @override
-  _TabPageState createState() => _TabPageState();
+  _HyundaiScreenState createState() => _HyundaiScreenState();
 }
 
-class _TabPageState extends State<TabPage> with TickerProviderStateMixin {
+class _HyundaiScreenState extends State<HyundaiScreen> with TickerProviderStateMixin {
   late TabController _tabController;
 
   @override
   void initState() {
     _tabController = TabController(
-      length: 6,
+      length: 16,
       vsync: this, //vsync에 this 형태로 전달해야 애니메이션이 정상 처리됨
     );
     super.initState();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +40,23 @@ class _TabPageState extends State<TabPage> with TickerProviderStateMixin {
               indicatorWeight: 2,
               isScrollable: true,
               tabs: [
-                tabInfo(context, '내차량 최근  보기 '),
-                tabInfo(context, '엔진오일'),
-                tabInfo(context, '핸들오일'),
-                tabInfo(context, '브레이크오일'),
-                tabInfo(context, '와이퍼'),
-                tabInfo(context, '냉각수'),
+                tabInfo( '내차량 최근  보기'),
+                tabInfo('오일'),
+                tabInfo( '플러싱(세정)'),
+                tabInfo( '플러그'),
+                tabInfo( '벨트'),
+                tabInfo( '체인 방식 워터펌프 베어링 set'),
+                tabInfo( '변속기 오일 '),
+                tabInfo( '연료휠터'),
+                tabInfo( '인젝터클리닝'),
+                tabInfo( '브레이크'),
+                tabInfo( '부동액'),
+                tabInfo( '에어컨 과 필터'),
+                tabInfo('파워스티터어링 오일'),
+                tabInfo('밧데리'),
+                tabInfo('하체 언더코팅 '),
+                tabInfo( '와이퍼'),
+
               ],
               indicator: BoxDecoration(
                 shape: BoxShape.rectangle,
@@ -54,8 +66,8 @@ class _TabPageState extends State<TabPage> with TickerProviderStateMixin {
                   begin: Alignment.centerLeft,
                   end: Alignment.centerRight,
                   colors: [
-                    Colors.blueAccent,
-                    Colors.black,
+                    Colors.blue,
+                    Colors.indigo,
                   ],
                 ),
               ),
@@ -70,10 +82,21 @@ class _TabPageState extends State<TabPage> with TickerProviderStateMixin {
               children: [
                 tabViewInfo(''),
                 tabViewInfo(engineOil),
-                tabViewInfo(''),
-                tabViewInfo(''),
-                tabViewInfo(''),
-                tabViewInfo(''),
+                tabViewInfo(cleaningFlushing),
+                tabViewInfo(sparkPlug),
+                tabViewInfo(timingBelt),
+                tabViewInfo(waterPumpBearingSet),
+                tabViewInfo(engineTransmissionMounting),
+                tabViewInfo(fuelFilter),
+                tabViewInfo(fuelInjectorCleaning),
+                tabViewInfo(brake),
+                tabViewInfo(antifreeze),
+                tabViewInfo(airCondition),
+                tabViewInfo(wheelAlignment),
+                tabViewInfo(battery),
+                tabViewInfo(lowerBodyUndercoating),
+                tabViewInfo(wiper),
+
               ],
             ),
           ),
@@ -83,9 +106,9 @@ class _TabPageState extends State<TabPage> with TickerProviderStateMixin {
   }
 }
 
-Widget tabInfo(BuildContext context, String title) {
+Widget tabInfo( String title) {
   return Container(
-    height: MediaQuery.of(context).size.height * 0.06,
+    height: 60,
     alignment: Alignment.center,
     child: Text(
       title,
@@ -94,13 +117,31 @@ Widget tabInfo(BuildContext context, String title) {
 }
 
 Widget tabViewInfo(String content) {
-  return Container(
-    alignment: Alignment.center,
-    child: Text(
-      content,
-      style: TextStyle(
-        fontSize: 20,
+  return ListView(
+    children: [
+      Container(
+        alignment: Alignment.center,
+        child: Text(
+          content,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
       ),
-    ),
+    ],
   );
+}
+
+class CarPeriodicListScreen extends StatefulWidget {
+  const CarPeriodicListScreen({super.key});
+
+  @override
+  State<CarPeriodicListScreen> createState() => _CarPeriodicListScreenState();
+}
+
+class _CarPeriodicListScreenState extends State<CarPeriodicListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
