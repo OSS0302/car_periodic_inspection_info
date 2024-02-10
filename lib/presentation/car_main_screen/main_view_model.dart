1c08b7c01b0270c7181989b8d3b20bfd95911769
OSS0302 <tkdtn3264@gmail.com>
2024-02-10 20:02:37 +0900

    비지니스 로직 뷰모델로 옮김

--- lib/presentation/car_main_screen/main_view_model.dart
+++ lib/presentation/car_main_screen/main_view_model.dart
@@ -1,11 +1,19 @@
+import 'dart:async';
+
 import 'package:car_periodic_inspection_info/data/repository/hyundi_mock_repositoryimpl.dart';
 import 'package:car_periodic_inspection_info/domain/repository/car_periodic_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MainViewModel extends ChangeNotifier {
-  bool _isLoading = false;
+  String? userUid;
+  bool _isLoading = true;
   bool? isChecked = false;
+  bool isComplete = false;
+  final supabase = Supabase.instance.client;
+  StreamSubscription? _streamSubscription;
   CarPeriodicRepository _repository;
+  List<Map<String, dynamic>> _data = [];
 
   bool get isLoading => _isLoading;
 
@@ -21,5 +29,45 @@ class MainViewModel extends ChangeNotifier {
     notifyListeners();
     _isLoading = false;
     notifyListeners();
+  }
+
+  @override
+  void initState() {
+    initializeUserInfoAndSubscribeToChanges();
+    notifyListeners();
+  }
+
+  Future<void> initializeUserInfoAndSubscribeToChanges() async {
+    final user = supabase.auth.currentUser;
+    if (user != null) {
+        userUid = user.id;
+
+      subscribeToUserChanges(user.id);
+      notifyListeners();
+    }
+  }
+
+  void subscribeToUserChanges(String userId) {
+    _streamSubscription = supabase
+        .from('car_periodic_add')
+        .stream(primaryKey: ['id'])
+        .eq('uid', userId)
+        .order('date', ascending: false)
+        .listen((data) {
+
+        _data = data;
+        _isLoading = false;
+        notifyListeners();
+    }, onError: (error) {
+
+        _isLoading = false;
+        notifyListeners();
+    });
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription?.cancel();
+    super.dispose();
   }
 }
