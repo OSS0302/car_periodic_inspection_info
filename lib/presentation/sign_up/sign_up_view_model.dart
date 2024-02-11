7444744dc0810eaf950ed929576c06ef1f575f2a
OSS0302 <tkdtn3264@gmail.com>
2024-02-11 19:38:49 +0900

    회원가입 뷰모델 작성

---
+++ lib/presentation/sign_up/sign_up_view_model.dart
@@ -0,0 +1,49 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../car_main_screen/main_screen.dart';
+
+class SignInUpwModel extends ChangeNotifier {
+
+
+  String getId = '';
+  String getPass = '';
+  String getNamePass = '';
+  String getPhonePass = '';
+
+  void setValue({
+    String? idString,
+    String? passwordString,
+    String? namePass,
+    String? phonePass,
+  }) {
+    getId = idString ?? getId;
+    getPass = passwordString ?? getPass;
+    getNamePass = namePass ?? getNamePass;
+    getPhonePass = phonePass ?? getPhonePass;
+
+    log('getId: $getId, getPass: $getPass, getNamePass: $getNamePass, getPhonePass: $getPhonePass');
+  }
+
+  Future<bool> signupSupabase() async {
+    final AuthResponse res = await supabase.auth.signUp(
+      email: getId,
+      password: getPass,
+      data: {
+        'name': getNamePass,
+        'phone': getPhonePass,
+      },
+    );
+    if (res.user != null) {
+      await supabase.from('user_info').insert({
+        'name': getNamePass,
+        'phone': getPhonePass,
+      });
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
